7335c15cbcce254463d219ed5a436c0e6f3ce098
Edu Canales <131183212+educanales@users.noreply.github.com>
2024-09-14 14:12:13 -0300

    Add event listener to the button and create function to remove squares

--- script.js
+++ script.js
@@ -3,13 +3,21 @@
 */
 
 const container = document.querySelector(".container");
-let squaresDivs = 16
+const squaresBtn = document.querySelector(".squares-btn");
+let squaresDivs = 16;
+// let squareSize = 640 / squaresDivs;
+// squareSize = (squareSize.toString()+"px");
 
+// console.log(squaresDivs)
 
 function generateDivs(num) {
   for (let i= 0; i < num * num; i++) {
     const div = document.createElement("div");
-    div.classList.add("new-div");
+    let squareSize = 640 / squaresDivs;
+    squareSize = (squareSize.toString()+"px");
+    // div.classList.add("new-div");
+    div.style.width = squareSize;
+    div.style.height = squareSize;
     container.appendChild(div);
     div.addEventListener('mouseover', () => {
       div.style.backgroundColor = "black";
@@ -17,5 +25,20 @@ function generateDivs(num) {
   }
 }
 
+function removeDivs() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+
 generateDivs(squaresDivs);
 
+squaresBtn.addEventListener('click', () => {
+  removeDivs();
+  squaresDivs = prompt( "How many squares per side do you want? (Max. 100): " );
+  squaresDivs = Number(squaresDivs);
+  if (squaresDivs > 100) {
+    squaresDivs = prompt( "Please enter a number less than 100: " );
+  }
+  generateDivs(squaresDivs);
+})
